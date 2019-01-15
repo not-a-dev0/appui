4ebe22950cae06fa95ffd60d7ad747fb57fa507e
Adarsh Dubey <Adarshdubeytech@gmail.com>
2019-01-15 16:09:46 +0530

    Add

---
+++ src/api/auth.js
@@ -0,0 +1,23 @@
+import axiosInstance from './requestBuilder';
+
+
+export const logoutUser = () => {
+  return axiosInstance.delete('/api/v2/barong/identity/sessions')
+    .then(response => response.data)
+};
+
+export const loginUser = (email, password, otp_code='', recaptcha_response='') => {
+  return axiosInstance.post(
+      '/api/v2/barong/identity/sessions',
+      { email, password, otp_code, recaptcha_response }
+    )
+    .then(response => response.data)
+};
+
+export const signupUser = (email, password, recaptcha_response='') => {
+  return axiosInstance.post(
+      '/api/v2/barong/identity/users',
+      { email, password, recaptcha_response }
+    )
+    .then(response => response.data)
+};
